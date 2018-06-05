bd5162bac7f9481e0db0f76a6706338c4990d9b2
sumitdevops <sumittuljapurkar14@gmail.com>
2018-06-05 19:28:53 +0530

    hi new changes

---
+++ src/HelloWorld.java
@@ -0,0 +1,23 @@
+
+public class HelloWorld {
+	
+	static {
+	    System.out.println("static block2");
+		}static {
+		    System.out.println("static block3");
+			}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+System.out.println("Hello world! how are you");
+
+	}
+	
+	static {
+	    System.out.println("static block1");
+		}
+	
+	
+
+}
